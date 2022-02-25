6884605593d62e359b43940b0795ff3feb3a76a1
aYunes <atessmannyunes@gmail.com>
2022-02-25 20:07:35 -0300

    add timeline toggle details logic

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -12,4 +12,30 @@ export class TimelineComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showFirstDate = false;
+  showSecondDate = false;
+  showThirdDate = false;
+  showFourthDate = false;
+  showFifthDate = false;
+
+  toggleFirstDate(){
+    this.showFirstDate = !this.showFirstDate;
+  }
+
+  toggleSecondDate(){
+    this.showSecondDate = !this.showSecondDate;
+  }
+
+  toggleThirdtDate(){
+    this.showThirdDate = !this.showThirdDate;
+  }
+
+  toggleFourthDate(){
+    this.showFourthDate = !this.showFourthDate;
+  }
+
+  toggleFifthDate(){
+    this.showFifthDate = !this.showFifthDate;
+  }
+
 }
